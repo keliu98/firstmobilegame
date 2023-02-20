2bb1dff1ca452296a9ff37928e0a97800c50a8f0
Dania Mohd <44768559+DaniaMohd@users.noreply.github.com>
2023-02-20 22:12:28 +0800

    Documentation

--- app/src/main/java/com/example/firstgame/GameThread.kt
+++ app/src/main/java/com/example/firstgame/GameThread.kt
@@ -3,11 +3,18 @@ package com.example.firstgame
 import android.graphics.Canvas
 import android.view.SurfaceHolder
 
+/**
+ * Game Thread is an active process that always runs. This is where the main game loop will keep looping.
+ *
+ */
 class GameThread(private val surfaceHolder: SurfaceHolder, private val gameView: GameView) : Thread() {
-    private var running: Boolean = false
+    private var running: Boolean = false //sets if thread should be running, useful for pause state
 
     private val targetFPS = 60 // frames per second, the rate at which you would like to refresh the Canvas
 
+    /**
+     * Set if you want to run or stop.
+     */
     fun setRunning(isRunning: Boolean) {
         this.running = isRunning
     }
@@ -25,16 +32,25 @@ class GameThread(private val surfaceHolder: SurfaceHolder, private val gameView:
             try {
                 // locking the canvas allows us to draw on to it
                 canvas = this.surfaceHolder.lockCanvas()
+
+                /**
+                 * This is where the whole Game Loops!
+                 */
                 synchronized(surfaceHolder) {
                     this.gameView.GameLoop()
                     this.gameView.draw(canvas!!)
                 }
+
             } catch (e: Exception) {
                 e.printStackTrace()
             } finally {
                 if (canvas != null) {
                     try {
+                        /**
+                         * Unlocks canvas for our use!
+                         */
                         surfaceHolder.unlockCanvasAndPost(canvas)
+
                     } catch (e: Exception) {
                         e.printStackTrace()
                     }
@@ -45,7 +61,7 @@ class GameThread(private val surfaceHolder: SurfaceHolder, private val gameView:
             waitTime = targetTime - timeMillis
 
             try {
-                sleep(waitTime)
+                sleep(waitTime) //Wait until the full 1/60s is up before running game loop again.
             } catch (e: Exception) {
                 e.printStackTrace()
             }
